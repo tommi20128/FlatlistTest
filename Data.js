86bf3d27b573e14a32f9d93b1618b7dfed504ab7
tommi20128 <70171019+tommi20128@users.noreply.github.com>
2023-09-27 11:36:20 +0300

    Lisätty henkilöitä

--- Data.js
+++ Data.js
@@ -12,11 +12,66 @@ export const DATA = [
     {
         id: '3',
         lastname: 'Myllyoja',
-        firstname: 'Antti',
+        firstname: 'Antti'
     },
     {
         id: '4',
         lastname: 'Ojanperä',
         firstname: 'Tuomas'
+    },
+    {
+        id: '5',
+        lastname: 'Paakkolanvaara',
+        firstname: 'Tommi'
+    },
+    {
+        id: '6',
+        lastname: 'Autio',
+        firstname: 'Elina'
+    },
+    {
+        id: '7',
+        lastname: 'Myllyoja',
+        firstname: 'Antti'
+    },
+    {
+        id: '8',
+        lastname: 'Ojanperä',
+        firstname: 'Tuomas'
+    },
+    {
+        id: '9',
+        lastname: 'Paakkolanvaara',
+        firstname: 'Tommi'
+    },
+    {
+        id: '10',
+        lastname: 'Autio',
+        firstname: 'Elina'
+    },
+    {
+        id: '11',
+        lastname: 'Myllyoja',
+        firstname: 'Antti'
+    },
+    {
+        id: '12',
+        lastname: 'Ojanperä',
+        firstname: 'Tuomas'
+    },
+    {
+        id: '13',
+        lastname: 'Paakkolanvaara',
+        firstname: 'Tommi'
+    },
+    {
+        id: '14',
+        lastname: 'Autio',
+        firstname: 'Elina'
+    },
+    {
+        id: '15',
+        lastname: 'Myllyoja',
+        firstname: 'Antti'
     }
 ]
